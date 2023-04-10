c4cf9008b3a999c11f37c4e178f98582052a5f45
OnlyNumber <98099864+OnlyNumber@users.noreply.github.com>
2023-04-10 12:08:56 +0300

    Initialize

---
+++ Survoriors/Assets/Scripts/Launcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        Debug.Log("Присоединяемся к Master Server");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+
+        Debug.Log("Connect to Master Server");
+    }
+
+    public override void OnJoinedLobby()
+    {
+
+        Debug.Log("Присоединились к Master Server");
+
+    }
+}
